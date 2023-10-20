8706c6274ef8f78ed6623690469300a0d3c31d09
Christie Beaubrun <christiebeaubrun02@gmail.com>
2023-10-20 18:50:55 -0400

    L

--- ios101-lab5-flix1/ViewController.swift
+++ ios101-lab5-flix1/ViewController.swift
@@ -7,12 +7,35 @@ import UIKit
 import Nuke
 
 // TODO: Add table view data source conformance
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    // A property to store the movies we fetch.
+    // Providing a default value of an empty array (i.e., `[]`) avoids having to deal with optionals.
+    private var movies: [Movie] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = UITableViewCell()
 
+            // Get the movie-associated table view row
+            let movie = movies[indexPath.row]
+
+            // Configure the cell (i.e., update UI elements like labels, image views, etc.)
+            cell.textLabel?.text = movie.title
+
+            // Return the cell for use in the respective table view row
+            return cell
+    }
+    
+    
+    
 
     // TODO: Add table view outlet
 
-
+    @IBOutlet weak var TableView: UITableView!
+    
     // TODO: Add property to store fetched movies array
 
 
@@ -20,8 +43,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         // TODO: Assign table view data source
-
-
+        TableView.dataSource = self
+        
         fetchMovies()
     }
 
@@ -67,6 +90,8 @@ class ViewController: UIViewController {
 
                 // Run any code that will update UI on the main thread.
                 DispatchQueue.main.async { [weak self] in
+                    
+                    self?.movies = movies
 
                     // We have movies! Do something with them!
                     print("✅ SUCCESS!!! Fetched \(movies.count) movies")
